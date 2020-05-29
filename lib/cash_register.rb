d22792b274d31987b6277ca65b08bacd7ce4aa2f
Jeff Bai <j.bai@wustl.edu>
2020-05-29 21:55:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total
+  attr_accessor :total, :discount, :last_tx_amount
   def initialize(discount = 0)
     @total = 0
     @discount = discount
