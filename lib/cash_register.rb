7ca0c43a2c8a767f6715ae062e9c99b1c91f35a1
Jeff Bai <j.bai@wustl.edu>
2020-05-29 21:35:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 class CashRegister
   attr_accessor :total
-  def initialize(total = 0)
-    @total = total
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
   end
 
   def discount
